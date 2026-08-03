d12dfaa01239539ce366d6218e4a6a5abc2421fd
sanjay majumdar <sanjaymajumder039@gmail.com>
2026-08-03 10:12:25 +0530

    Added scroll reveal animations with JavaScript

---
+++ script.js
@@ -0,0 +1,19 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+
+    reveals.forEach(section => {
+
+        const top = section.getBoundingClientRect().top;
+
+        const windowHeight = window.innerHeight;
+
+        if(top < windowHeight - 100){
+
+            section.classList.add("active");
+
+        }
+
+    });
+
+});
